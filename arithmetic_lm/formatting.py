ac37f743a1bf3a0e8b4cf78e3e1110a4a7aef719
iibrahimli <imranibrahimli98@gmail.com>
2024-02-25 21:11:22 +0100

    Remove docstring

--- arithmetic_lm/formatting.py
+++ arithmetic_lm/formatting.py
@@ -1,9 +1,5 @@
 """
-Input formatting types for arithmetic tasks:
- - Plain: standard formatting of addition
- - Reverse: flips the order of the output and encapsulates each data sample with the '$' symbol at the start and end.
- - Simplified Scratchpad: provides carry and digit-sum information for each step of addition, from the LSB to the MSB.
- - Detailed Scratchpad: provides explicit details of intermediate steps of addition.
+Input formatting types for arithmetic tasks
 """
 
 PLAIN_FORMAT_STR = "{a}{op}{b}={ans}\n"
