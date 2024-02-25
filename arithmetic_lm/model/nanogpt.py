cc44dfab4e8a95f1eabfa0590b8adc77744efae7
iibrahimli <imranibrahimli98@gmail.com>
2024-02-25 21:13:18 +0100

    Try without init weight

--- arithmetic_lm/model/nanogpt.py
+++ arithmetic_lm/model/nanogpt.py
@@ -95,13 +95,13 @@ class NanoGPT(nn.Module):
         self.lm_head.weight = self.embedding.weight
 
         # init all weights
-        self.apply(self._init_weights)
-        # apply special scaled init to the residual projections, per GPT-2 paper
-        for pn, p in self.named_parameters():
-            if pn.endswith("self_attn.out_proj.weight"):
-                torch.nn.init.normal_(
-                    p, mean=0.0, std=0.02 / math.sqrt(2 * self.n_layers)
-                )
+        # self.apply(self._init_weights)
+        # # apply special scaled init to the residual projections, per GPT-2 paper
+        # for pn, p in self.named_parameters():
+        #     if pn.endswith("self_attn.out_proj.weight"):
+        #         torch.nn.init.normal_(
+        #             p, mean=0.0, std=0.02 / math.sqrt(2 * self.n_layers)
+        #         )
 
     def _init_weights(self, module: nn.Module):
         if isinstance(module, nn.Linear):
