7bcc8749ba6bcf1e9fcf0ec9a28758e36e6db1c8
iibrahimli <imranibrahimli98@gmail.com>
2024-02-25 21:11:32 +0100

    simplify eval_on_batch

--- arithmetic_lm/eval_utils.py
+++ arithmetic_lm/eval_utils.py
@@ -15,17 +15,15 @@ def eval_on_batch(
     model, tokenizer: Tokenizer, batch: tuple[torch.Tensor, torch.Tensor], **gen_kwargs
 ) -> dict:
     """Returns dict"""
-    correct = 0
-    total = 0
-
     prompt, answer = batch
     if prompt.ndim == 1:
         prompt = prompt.unsqueeze(0)
     pred_answer = model.generate(prompt, max_new_tokens=answer.numel(), **gen_kwargs)
-    correct += eval_sample(
-        tokenizer.decode(pred_answer.squeeze().tolist()),
-        tokenizer.decode(answer.squeeze().tolist()),
+    correct = int(
+        eval_sample(
+            tokenizer.decode(pred_answer.squeeze().tolist()),
+            tokenizer.decode(answer.squeeze().tolist()),
+        )
     )
-    total += 1
 
-    return {"accuracy": correct / total, "correct": correct, "total": total}
+    return {"accuracy": correct}
