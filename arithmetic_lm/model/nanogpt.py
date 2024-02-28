48983b51248d6d5db6a84cb3b1fd3ff9f712a658
iibrahimli <imranibrahimli98@gmail.com>
2024-02-28 16:23:11 +0100

    fix sqrt

--- arithmetic_lm/model/nanogpt.py
+++ arithmetic_lm/model/nanogpt.py
@@ -94,7 +94,7 @@ class NanoGPT(nn.Module):
         Returns:
             logits: Tensor, shape ``[batch_size, seq_len, vocab_size]``
         """
-        x = self.embedding(x) / torch.sqrt(self.n_embd)
+        x = self.embedding(x) / (self.n_embd**0.5)
         x = self.pos_encoder(x)
 
         x = self.transformer_encoder(
