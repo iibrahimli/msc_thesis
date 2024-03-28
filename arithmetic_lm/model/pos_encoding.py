345de487bc9292c1d7bd95dd30b53d978505bbd0
iibrahimli <imranibrahimli98@gmail.com>
2024-03-28 15:55:53 +0100

    add class

--- arithmetic_lm/model/pos_encoding.py
+++ arithmetic_lm/model/pos_encoding.py
@@ -1,11 +1,15 @@
-# Since relative positional encoding implementation changes the multi-head attention layer,
-# we need to child class the original MHA and override the forward method to include it.
-
-
 import math
+from typing import Optional
 
 import torch
-from torch import Tensor, nn
+from torch import Tensor
+from torch import functional as F
+from torch import nn
+from torch.nn.modules.activation import (
+    _arg_requires_grad,
+    _check_arg_device,
+    _is_make_fx_tracing,
+)
 
 
 # from https://pytorch.org/tutorials/beginner/transformer_tutorial.html
@@ -60,3 +64,39 @@ class CoordinateEncoding(nn.Module):
         x[:, :, 0::2] += torch.sin(timestep * self.div_term)
         x[:, :, 1::2] += torch.cos(timestep * self.div_term)
         return self.dropout(x)
+
+
+class RelativePositionalEncoding(nn.Module):
+    pass
+
+
+# Since relative positional encoding implementation changes the multi-head attention layer,
+# we need to child class the original MHA and override the forward method to include it.
+class RelativeMultiheadAttention(nn.MultiheadAttention):
+    def forward(
+        self,
+        query: Tensor,
+        key: Tensor,
+        value: Tensor,
+        **kwargs,
+    ) -> tuple[Tensor, Optional[Tensor]]:
+        """
+        query: Query embeddings of shape :math:`(L, E_q)` for unbatched input, :math:`(L, N, E_q)` when ``batch_first=False``
+            or :math:`(N, L, E_q)` when ``batch_first=True``, where :math:`L` is the target sequence length,
+            :math:`N` is the batch size, and :math:`E_q` is the query embedding dimension ``embed_dim``.
+            Queries are compared against key-value pairs to produce the output.
+            See "Attention Is All You Need" for more details.
+        key: Key embeddings of shape :math:`(S, E_k)` for unbatched input, :math:`(S, N, E_k)` when ``batch_first=False``
+            or :math:`(N, S, E_k)` when ``batch_first=True``, where :math:`S` is the source sequence length,
+            :math:`N` is the batch size, and :math:`E_k` is the key embedding dimension ``kdim``.
+            See "Attention Is All You Need" for more details.
+        value: Value embeddings of shape :math:`(S, E_v)` for unbatched input, :math:`(S, N, E_v)` when
+            ``batch_first=False`` or :math:`(N, S, E_v)` when ``batch_first=True``, where :math:`S` is the source
+            sequence length, :math:`N` is the batch size, and :math:`E_v` is the value embedding dimension ``vdim``.
+            See "Attention Is All You Need" for more details.
+        """
+        # add relative positions to keys and values
+        # TODO
+
+        # call the parent class forward method
+        return super().forward(query, key, value, **kwargs)
