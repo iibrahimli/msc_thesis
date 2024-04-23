d292c160bddf15805fd75d6864e92abac9dd267d
iibrahimli <imranibrahimli98@gmail.com>
2024-04-23 14:11:14 +0200

    hasattr

--- arithmetic_lm/model/utils.py
+++ arithmetic_lm/model/utils.py
@@ -5,7 +5,7 @@ from torch import nn
 def init_weights(module: nn.Module):
     if isinstance(module, (nn.Linear, nn.Embedding)):
         torch.nn.init.xavier_uniform_(module.weight)
-        if module.bias is not None:
+        if hasattr(module, "bias") and module.bias is not None:
             torch.nn.init.zeros_(module.bias)
     # elif isinstance(module, nn.Embedding):
     #     torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
