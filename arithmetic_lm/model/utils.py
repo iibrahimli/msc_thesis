5a931d89ac147f7625001ea0b637d60732b9b1d1
iibrahimli <imranibrahimli98@gmail.com>
2024-04-27 15:15:38 +0200

    Init embedding weights with N(0, 0.02)

--- arithmetic_lm/model/utils.py
+++ arithmetic_lm/model/utils.py
@@ -3,9 +3,9 @@ from torch import nn
 
 
 def init_weights(module: nn.Module):
-    if isinstance(module, (nn.Linear, nn.Embedding)):
+    if isinstance(module, nn.Linear):
         torch.nn.init.xavier_uniform_(module.weight)
         if hasattr(module, "bias") and module.bias is not None:
             torch.nn.init.zeros_(module.bias)
-    # elif isinstance(module, nn.Embedding):
-    #     torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+    elif isinstance(module, nn.Embedding):
+        torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
