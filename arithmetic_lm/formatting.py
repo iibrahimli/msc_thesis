177054060289ec172eccd0dd52f4276d87e32bf3
iibrahimli <imranibrahimli98@gmail.com>
2024-05-06 00:44:50 +0200

    random zero pad

--- arithmetic_lm/formatting.py
+++ arithmetic_lm/formatting.py
@@ -2,6 +2,7 @@
 Input formatting types for arithmetic tasks
 """
 
+import random
 import re
 
 PLAIN_FORMAT_STR = "{a}{op}{b}={ans}\n"
@@ -21,13 +22,15 @@ def format_line(
     pad_ans_zero: int | None = None,
     prepend_newline: bool = False,
     append_newline: bool = False,
+    random_zero_padding: bool = True,
 ) -> str:
     """
     Format line based on args, assumes line has ends with \n,
     returned line ends with \n as well. pad_ans_zero is the number of digits
     to pad with zeros to, e.g. 43 -> 043 if pad_ans_zero=3. pad_ops_zero is
     the number of digits to pad the operands with zeros to, e.g. 43+3 -> 043+003
-    if pad_ops_zero=3.
+    if pad_ops_zero=3. If random_zero_padding is True, pad operands and answers
+    with a random number of zeros between length of number and pad_*_zero.
     """
 
     ab, ans = line.split("=")
@@ -35,8 +38,16 @@ def format_line(
     if pad_ops_zero:
         # split by non-digit char and pad operands with zeros
         a, op, b = split_operands_and_op(ab)
-        a = a.zfill(pad_ops_zero)
-        b = b.zfill(pad_ops_zero)
+        a = a.zfill(
+            pad_ops_zero
+            if not random_zero_padding
+            else random.randint(len(a), pad_ops_zero)
+        )
+        b = b.zfill(
+            pad_ops_zero
+            if not random_zero_padding
+            else random.randint(len(b), pad_ops_zero)
+        )
         ab = f"{a}{op}{b}"
 
     ab = ab.lstrip()
@@ -46,7 +57,11 @@ def format_line(
         ans = ans[::-1]
 
     if pad_ans_zero:
-        ans = ans.zfill(pad_ans_zero)
+        ans = ans.zfill(
+            pad_ans_zero
+            if not random_zero_padding
+            else random.randint(len(ans), pad_ans_zero)
+        )
 
     pad = pad if pad else ""
     res = f"{pad}{ab}={ans}{pad}"
