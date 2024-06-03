0e9f0a8a710e99e42b8e4e8192b02972a9c9ac4f
iibrahimli <imranibrahimli98@gmail.com>
2024-06-03 20:00:31 +0200

    Add abacus encoding

--- arithmetic_lm/model/transformer.py
+++ arithmetic_lm/model/transformer.py
@@ -1,7 +1,11 @@
 import torch
 from torch import Tensor, nn
 
-from .pos_encoding import AbsolutePositionalEncoding, RelativeMultiheadAttention
+from .pos_encoding import (
+    AbacusEncoding,
+    AbsolutePositionalEncoding,
+    RelativeMultiheadAttention,
+)
 from .utils import init_weights
 
 
@@ -45,9 +49,14 @@ class Transformer(nn.Module):
 
         # embedding (TODO: hardcoded pad index for char tokenizer)
         self.embedding = nn.Embedding(vocab_size, n_embd, padding_idx=99)
-        self.pos_encoder = AbsolutePositionalEncoding(
-            n_embd, max_len=context_len, dropout=dropout
-        )
+        if self.pos_enc == "abs":
+            self.pos_encoder = AbsolutePositionalEncoding(
+                n_embd, max_len=context_len, dropout=dropout
+            )
+        elif self.pos_enc == "abacus":
+            self.pos_encoder = AbacusEncoding(
+                embedding_dim=n_embd, max_seq_length=context_len
+            )
 
         self.encoder_layer = nn.TransformerEncoderLayer(
             d_model=n_embd,
