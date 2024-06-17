a09a419bb4333d7db8cd62413953bc473bdbe358
iibrahimli <imranibrahimli98@gmail.com>
2024-06-18 00:33:08 +0200

    Fix variable name

--- arithmetic_lm/model/rotary_pos_encoding.py
+++ arithmetic_lm/model/rotary_pos_encoding.py
@@ -335,7 +335,7 @@ class RotaryMultiheadAttention(nn.MultiheadAttention):
 
         # reshape query and key into [batch, n_heads, seq_len, d_head]
         query, key = [
-            rearrange(x, "b l (h d) -> b h l d", h=self.nhead) for x in (query, key)
+            rearrange(x, "b l (h d) -> b h l d", h=self.num_heads) for x in (query, key)
         ]
 
         # apply rotary positional encoding to queries and keys
