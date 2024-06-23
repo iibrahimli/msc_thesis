7a7af75a9826e0729e71eac8e9d75c8a680dc035
iibrahimli <imranibrahimli98@gmail.com>
2024-06-23 18:28:33 +0200

    Xavier uniform init

--- arithmetic_lm/model/utils.py
+++ arithmetic_lm/model/utils.py
@@ -3,10 +3,17 @@ from torch import nn
 
 
 def init_weights(module: nn.Module):
-    pass
+    # pass
+
+    # from NanoGPT:
     # if isinstance(module, nn.Linear):
     #     torch.nn.init.xavier_uniform_(module.weight)
     #     if hasattr(module, "bias") and module.bias is not None:
     #         torch.nn.init.zeros_(module.bias)
     # elif isinstance(module, nn.Embedding):
     #     torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+
+    # from torch Transformer:
+    for p in module.parameters():
+        if p.dim() > 1:
+            nn.init.xavier_uniform_(p)
